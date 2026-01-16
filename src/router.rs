8edd46cd662d5cb48f260b6beccff7b4d5655a5c
ramblestsad <yilinw6@gmail.com>
2026-01-16 15:37:44 -0800

    feat: implement router with index and health check routes

--- src/router.rs
+++ src/router.rs
@@ -1,6 +1,18 @@
 use axum::Router;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::get;
 
 pub async fn router() -> Result<Router, anyhow::Error> {
     // app init
-    Ok(Router::new())
+    Ok(Router::new()
+        .route("/", get(index))
+        .route("/health-check", get(health_check)))
+}
+
+async fn index() -> impl IntoResponse {
+    "Temporary Index Result"
+}
+async fn health_check() -> impl IntoResponse {
+    (StatusCode::OK, "duuuuu~")
 }
